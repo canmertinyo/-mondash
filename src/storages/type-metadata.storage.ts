ac563b359d5dce4864d5758b6bfc6c18b9138120
Can Çevik <mazlumcancevik@gmail.com>
2023-01-19 17:14:58 +0300

    update type metadata storage

--- src/storages/type-metadata.storage.ts
+++ src/storages/type-metadata.storage.ts
@@ -1,10 +1,20 @@
-import { PropertyMetadata } from '../metadata'
+import { Type } from '../interfaces'
+import { PropertyMetadata, SchemaMetadata } from '../metadata'
 
 class TypeMetadataStorageHost {
   private properties = new Array<PropertyMetadata>()
+  private schemas = new Array<SchemaMetadata>()
 
   public addPropertyMetadata(metadata: PropertyMetadata): void {
     this.properties.unshift(metadata)
+  }
+
+  public addSchemaMetadata(metadata: SchemaMetadata): void {
+    this.schemas.unshift(metadata)
+  }
+
+  public getSchemaMetadataByTarget(target: Type): SchemaMetadata | undefined {
+    return this.schemas.find((schema) => schema.target === target)
   }
 
   public getClassFieldsByPredicate(
